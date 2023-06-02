4d14729f8f989dd91340ca27d9d8c813426ab0d9
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-02 20:11:45 +0900

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="Header">
+      <a href="" className="logo">
+        <img src="./images/profile.png" alt="" />
+        <h1>PORI</h1>
+      </a>
+      <nav className="nav">
+        <button className="connect_wallet ">CONNECT WALLET</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;

554b38257ebc007e7fa009c629f65e40ec66a9fa
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-02 20:56:21 +0900

    connect wallet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,154 @@
+import { ethers } from "ethers";
+import { useCallback, useState, useEffect } from "react";
+
 function Header() {
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [walletAddress, setWalletAddress] = useState(undefined);
+  const [currentBalance, setCurrentBalance] = useState(undefined);
+  const [isConnected, setIsConnected] = useState(false);
+
+  const displayWalletAddress = `0x${walletAddress?.substring(
+    2,
+    5
+  )}...${walletAddress?.substring(38)}`;
+
+  const connectWallet = useCallback(async () => {
+    try {
+      //메타마스크 설치 된 경우
+      if (typeof window.ethereum !== "undefined") {
+        await getMetamaskData();
+        setIsConnected(true);
+        //메타마스크 설치 안된 경우
+      } else {
+        alert("please install MetaMask");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleChainChanged = async () => {
+    await getMetamaskData();
+  };
+
+  const getMetamaskData = async () => {
+    const _provider = await getProvider();
+    const _signer = await getSigner(_provider);
+    await getWalletData(_signer);
+
+    // 메타마스크 계정의 네트워크 ID가 변경되었을 때
+    window.ethereum.on("chainChanged", handleChainChanged);
+    window.ethereum.on("accountsChanged", handleAccountsChanged);
+  };
+
+  const handleAccountsChanged = async (accounts) => {
+    if (accounts.length === 0) {
+      // 계정이 없는 경우 처리
+    } else {
+      // 메타마스크로 선택된 계정 가져오기
+      const selectedAddress = accounts[0];
+
+      // provider, signer 업데이트
+      const _provider = await getProvider();
+      const _signer = await getSigner(_provider);
+
+      await getWalletData(_signer);
+
+      // 해당 계정으로 업데이트
+      setWalletAddress(selectedAddress);
+    }
+  };
+
+  const getProvider = async () => {
+    //메타마스크에서 제공하는 provider를 ethers 모듈에 저장
+    const provider = await new ethers.providers.Web3Provider(window.ethereum);
+
+    //상태변수 저장
+    setProvider(provider);
+
+    return provider;
+  };
+
+  const getSigner = async (provider) => {
+    //메타마스크에 홈페이지 연동 승인 요청
+    await provider.send("eth_requestAccounts", []);
+
+    //메타마스크로 서명 요청
+    const signer = provider.getSigner();
+    //서명 저장
+    setSigner(signer);
+
+    return signer;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await getWalletData(signer);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [signer]);
+
+  const getWalletData = async (signer) => {
+    const result = await Promise.all([
+      signer.getAddress(),
+      signer.getBalance(),
+    ]);
+    const newBalance = Number(ethers.utils.formatEther(result[1]));
+    if (newBalance !== currentBalance) {
+      setCurrentBalance(newBalance);
+    }
+
+    setWalletAddress(result[0]);
+  };
+
+  const handleAddressClick = () => {
+    const disconnectModal = document.querySelector(".disconnect-modal");
+    const disconnectDimm = document.querySelector(".disconnect-dimm");
+
+    disconnectModal.classList.toggle("disconnect-modal-on");
+    disconnectDimm.classList.toggle("disconnect-modal-on");
+  };
+
+  const disconnectWallet = useCallback(() => {
+    // 지갑 연결 해제 로직을 여기에 작성합니다.
+    setIsConnected(false);
+    handleAddressClick();
+  }, []);
+
   return (
-    <div className="Header">
+    <div className="header">
       <a href="" className="logo">
         <img src="./images/profile.png" alt="" />
         <h1>PORI</h1>
       </a>
       <nav className="nav">
-        <button className="connect_wallet ">CONNECT WALLET</button>
+        {isConnected ? (
+          <span className="buttonContainer" onClick={handleAddressClick}>
+            {displayWalletAddress} DISCONNECT
+          </span>
+        ) : (
+          <button className="connect_wallet " onClick={() => connectWallet()}>
+            CONNECT WALLET
+          </button>
+        )}
       </nav>
+      <div className="disconnect-modal">
+        <div className="disconnect-content">
+          <h2>Disconnect Wallet</h2>
+          <span>Are you sure you want to logout</span>
+        </div>
+        <div className="disconnect-btns">
+          <button className="disconnect-cancel" onClick={handleAddressClick}>
+            CANCEL
+          </button>
+          <button className="disconnect-button" onClick={disconnectWallet}>
+            DISCONNECT
+          </button>
+        </div>
+      </div>
+      <div className="disconnect-dimm" onClick={handleAddressClick}></div>
     </div>
   );
 }

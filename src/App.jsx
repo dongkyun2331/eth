74a03cbbec6cd18624085b422ece3e91bf31c489
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-03 09:29:43 +0900

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,135 @@
 import Header from "./components/Header";
 import Content from "./components/Content";
+import { useState, useCallback, useEffect } from "react";
+import { ethers } from "ethers";
 
 function App() {
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [walletAddress, setWalletAddress] = useState(undefined);
+  const [currentBalance, setCurrentBalance] = useState(undefined);
+  const [isConnected, setIsConnected] = useState(false);
+
+  const displayWalletAddress = `0x${walletAddress?.substring(
+    2,
+    5
+  )}...${walletAddress?.substring(38)}`;
+
+  const connectWallet = useCallback(async () => {
+    try {
+      //메타마스크 설치 된 경우
+      if (typeof window.ethereum !== "undefined") {
+        await getMetamaskData();
+        setIsConnected(true);
+        //메타마스크 설치 안된 경우
+      } else {
+        alert("please install MetaMask");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleChainChanged = async () => {
+    await getMetamaskData();
+  };
+
+  const getMetamaskData = async () => {
+    const _provider = await getProvider();
+    const _signer = await getSigner(_provider);
+    await getWalletData(_signer);
+
+    // 메타마스크 계정의 네트워크 ID가 변경되었을 때
+    window.ethereum.on("chainChanged", handleChainChanged);
+    window.ethereum.on("accountsChanged", handleAccountsChanged);
+  };
+
+  const handleAccountsChanged = async (accounts) => {
+    if (accounts.length === 0) {
+      // 계정이 없는 경우 처리
+    } else {
+      // 메타마스크로 선택된 계정 가져오기
+      const selectedAddress = accounts[0];
+
+      // provider, signer 업데이트
+      const _provider = await getProvider();
+      const _signer = await getSigner(_provider);
+
+      await getWalletData(_signer);
+
+      // 해당 계정으로 업데이트
+      setWalletAddress(selectedAddress);
+    }
+  };
+
+  const getProvider = async () => {
+    //메타마스크에서 제공하는 provider를 ethers 모듈에 저장
+    const provider = await new ethers.providers.Web3Provider(window.ethereum);
+
+    //상태변수 저장
+    setProvider(provider);
+
+    return provider;
+  };
+
+  const getSigner = async (provider) => {
+    //메타마스크에 홈페이지 연동 승인 요청
+    await provider.send("eth_requestAccounts", []);
+
+    //메타마스크로 서명 요청
+    const signer = provider.getSigner();
+    //서명 저장
+    setSigner(signer);
+
+    return signer;
+  };
+
+  const getWalletData = async (signer) => {
+    if (!signer) {
+      return;
+    }
+    const result = await Promise.all([
+      signer.getAddress(),
+      signer.getBalance(),
+    ]);
+    const newBalance = Number(ethers.utils.formatEther(result[1]));
+    if (newBalance !== currentBalance) {
+      setCurrentBalance(newBalance);
+    }
+
+    setWalletAddress(result[0]);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await getWalletData(signer);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [signer]);
+
+  const handleAddressClick = () => {
+    const disconnectModal = document.querySelector(".disconnect-modal");
+    const disconnectDimm = document.querySelector(".disconnect-dimm");
+
+    disconnectModal.classList.toggle("disconnect-modal-on");
+    disconnectDimm.classList.toggle("disconnect-modal-on");
+  };
+
+  const disconnectWallet = useCallback(() => {
+    // 지갑 연결 해제 로직을 여기에 작성합니다.
+    setIsConnected(false);
+    handleAddressClick();
+  }, []);
   return (
     <div>
-      <Header />
+      <Header
+        isConnected={isConnected}
+        handleAddressClick={handleAddressClick}
+        displayWalletAddress={displayWalletAddress}
+        connectWallet={connectWallet}
+        disconnectWallet={disconnectWallet}
+      />
       <Content />
     </div>
   );
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,46 @@
-function Content() {
-  return <div className="content">Content</div>;
+function Content(props) {
+  const { isConnected } = props;
+  return (
+    <div className="content">
+      <section className="section">
+        <div className="mint">
+          <h3>ETHEREUM</h3>
+        </div>
+        <div className="lavender">
+          <h3>MY BALANCE</h3>
+          <div className="balance">
+            <span>0.000000</span>
+            <p>ETH</p>
+          </div>
+          <div className="balance-btns">
+            <button className="deposit">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M12.3 6.7L8.5 2.9V15H7.5V2.9L3.7 6.7L3 6L8 1L13 6L12.3 6.7Z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              DEPOSIT
+            </button>
+            <button className="withdraw">
+              <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16}>
+                <path
+                  d="M13.2929 9.79295L11 12.0859V6C11 4.93913 10.5786 3.92172 9.82843 3.17157C9.07828 2.42143 8.06087 2 7 2C5.93913 2 4.92172 2.42143 4.17157 3.17157C3.42143 3.92172 3 4.93913 3 6V14H4V6C4 5.20435 4.31607 4.44129 4.87868 3.87868C5.44129 3.31607 6.20435 3 7 3C7.79565 3 8.55871 3.31607 9.12132 3.87868C9.68393 4.44129 10 5.20435 10 6V12.0859L7.70705 9.79295L7 10.5L10.5 14L14 10.5L13.2929 9.79295Z"
+                  fill="#222"
+                ></path>
+              </svg>
+              WITHDRAW
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Content;

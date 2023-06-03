d9b12e5c74f904bcf32c4cc6cf0958d3bfa845ed
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-03 18:45:25 +0900

    save

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,5 +1,31 @@
-function Community() {
-  return <div className="community">community</div>;
-}
+import { useEffect, useState } from "react";
+import Note from "./Note";
+import { db } from "../firebase";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 
-export default Community;
+export default function NoteList() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "notes"), orderBy("date", "desc"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({
+          ...doc.data(),
+          id: doc.id,
+        });
+      });
+      setNotes(items);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <ul className="list">
+      {notes.map((note) => (
+        <Note note={note} key={note.id} />
+      ))}
+    </ul>
+  );
+}

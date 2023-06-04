bd2100d8402daaaf2b120dd572f709b6bd669632
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-04 09:49:39 +0900

    ?

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -181,6 +181,7 @@ function ChatBot() {
       handleTicker(ticker);
       return;
     }
+
     if (inputText.includes("top10")) {
       fetch(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=24h"
